b56c9185d02d140d5446b0f46c2710cb6ae7c5a6
tyhuangg <anotherty2003@gmail.com>
2024-09-12 16:59:52 +0800

    update register function

--- cowrie_project/users/views.py
+++ cowrie_project/users/views.py
@@ -1,8 +1,32 @@
-from django.shortcuts import render
+# # Create your views here.
+# def register_view(request):
+#     form = UserCreationForm()
+#     return render(request, 'users/register.html', {"form":form})
 
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
+
 def register_view(request):
-    return render(request, 'users/register.html')
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            login(request, form.save()) #register same time as logging in
+            return redirect('homepage_view')  # 重定向到主页或其他页面
+    else:
+        form = UserCreationForm()
+    return render(request, 'users/register.html', {"form": form})
 
 def login_view(request):
     return render(request, 'users/login.html')
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('homepage_view')
+    else:
+        form = AuthenticationForm()
+    return render(request, "users/login.html", {"form": form })
