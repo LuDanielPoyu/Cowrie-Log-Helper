86cb58a65b1948880459a7f11d3bbb9e8f38d094
Your Name <hannahsu0421@gmail.comgit config --global user.name HsuHannagit config --global user.email hannahsu0421@gmail.com>
2024-09-17 21:18:00 +0800

    hanna's first try

--- cowrie_project/your_help_coach/views.py
+++ cowrie_project/your_help_coach/views.py
@@ -3,7 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def classify_log_view(request):
     return render(request, 'your_help_coach/classify_log.html')
-
 def attack_suggestion_view(request):
     return render(request, 'your_help_coach/attack_suggestion.html')
 
