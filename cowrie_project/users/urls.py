074813fc522edb032787dd1b0dec517e2c55eef6
yenrongchen <yenrong1040130@gmail.com>
2024-10-31 00:03:23 +0800

    update page title and add logout function

--- cowrie_project/users/urls.py
+++ cowrie_project/users/urls.py
@@ -7,4 +7,5 @@ app_name = 'users'
 urlpatterns = [
     path('register/', views.register_view, name="register"),
     path('login/', views.login_view, name="login"),
+    path('logout/', views.logout_view, name="logout"),
 ]
--- cowrie_project/users/views.py
+++ cowrie_project/users/views.py
@@ -1,34 +1,16 @@
-# # Create your views here.
-# def register_view(request):
-#     form = UserCreationForm()
-#     return render(request, 'users/register.html', {"form":form})
-
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib.auth import login
-
-#
-from django.shortcuts import render
-
-def classify_log(request):
-    return render(request, 'your_help_coach/classify_log.html')
-
-
-#
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout
 
 def register_view(request):
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            login(request, form.save()) #register same time as logging in
-            return redirect('homepage_view')  # 重定向到主页或其他页面
+            login(request, form.save()) # register same time as logging in
+            return redirect('homepage_view')
     else:
         form = UserCreationForm()
     return render(request, 'users/register.html', {"form": form})
-
-def login_view(request):
-    return render(request, 'users/login.html')
 
 def login_view(request):
     if request.method == "POST":
@@ -39,3 +21,8 @@ def login_view(request):
     else:
         form = AuthenticationForm()
     return render(request, "users/login.html", {"form": form })
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect('homepage_view')
