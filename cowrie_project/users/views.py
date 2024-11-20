323223daaf83d626260166a42998f3756b675a33
yenrongchen <yenrong1040130@gmail.com>
2024-11-21 00:36:25 +0800

    add email field when registering

--- cowrie_project/users/views.py
+++ cowrie_project/users/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import AuthenticationForm
+from .forms import CustomUserCreationForm
 
 def register_view(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
+
         if form.is_valid():
-            login(request, form.save()) 
-            return redirect('homepage_view')
+            email = form.cleaned_data.get("email").lower()
+
+            if User.objects.filter(email__iexact = email).exists():
+                form.add_error("email", "This email is already registered! Please use another one.")
+            else:
+                login(request, form.save(), backend = 'django.contrib.auth.backends.ModelBackend') # register same time as logging in
+                return redirect('homepage_view')
     else:
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {"form": form})
 
 def login_view(request):
     if request.method == "POST":
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
-            login(request, form.get_user())
+            login(request, form.get_user(), backend = 'django.contrib.auth.backends.ModelBackend')
             return redirect('homepage_view')
     else:
         form = AuthenticationForm()
