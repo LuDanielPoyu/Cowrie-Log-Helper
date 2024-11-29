144bb28a9d4cf281c3c52b0fee5689b59997fe00
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-11-29 16:54:37 +0800

    變更驗證信email來源

--- cowrie_project/cowrie_project/settings.py
+++ cowrie_project/cowrie_project/settings.py
@@ -150,8 +150,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com' 
 EMAIL_PORT = 587  
 EMAIL_USE_TLS = True 
-EMAIL_HOST_USER = 'fbi0826@gmail.com'  
-EMAIL_HOST_PASSWORD = 'atxyxbhknarrjpqr'  
+EMAIL_HOST_USER = 'misprojectg13@gmail.com'  
+EMAIL_HOST_PASSWORD = 'uirisxdnvdpdihat'  
 
 
 # Internationalization
