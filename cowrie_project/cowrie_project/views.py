39860addb1a02d340044ad6bd0904abf77efe40d
BensonFanNccu <110306044@g.nccu.edu.tw>
2024-12-03 19:19:44 +0800

    跑馬燈串接

--- cowrie_project/cowrie_project/views.py
+++ cowrie_project/cowrie_project/views.py
@@ -1,49 +1,23 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.db.models import Count
+from ask_me.models import ClassificationHistory
+
+import pandas as pd
 
 def homepage_view(request):
-     marquee_left = [
-        "cowrie.session.params: Collected 1968 records.",
-        "cowrie.session.file_upload: Collected 175 records.",
-        "cowrie.session.file_download.failed: Collected 243 records.",
-        "cowrie.session.file_download: Collected 111 records.",
-        "cowrie.session.closed: Collected 7973 records.",
-        "cowrie.login.success: Collected 3531 records.",
-        "cowrie.login.failed: Collected 2316 records.",
-        "cowrie.log.closed: Collected 1839 records.",
-        "cowrie.direct-tcpip.request: Collected 4388 records.",
-        "cowrie.direct-tcpip.data: Collected 341 records.",
-        "cowrie.command.input: Collected 2033 records.",
-        "cowrie.command.failed: Collected 190 records.",
-        "cowrie.client.version: Collected 98 records.",
-        "cowrie.client.size: Collected 76 records.",
-        "cowrie.client.kex: Collected 6650 records.",
-        "cowrie.session.connect: Collected 8419 records.",
-        "Collected 643 records in the past 7 days.",
-        "Collected an average of 2957 records per month."
-    ]
-     marquee_right = [
-        "cowrie.direct-tcpip.data: Collected 341 records.",
-        "cowrie.command.input: Collected 2033 records.",
-        "cowrie.command.failed: Collected 190 records.",
-        "cowrie.client.version: Collected 98 records.",
-        "cowrie.client.size: Collected 76 records.",
-        "cowrie.client.kex: Collected 6650 records.",
-        "cowrie.session.connect: Collected 8419 records.",
-        "Collected 643 records in the past 7 days.",
-        "Collected an average of 2957 records per month.",
-        "cowrie.session.params: Collected 1968 records.",
-        "cowrie.session.file_upload: Collected 175 records.",
-        "cowrie.session.file_download.failed: Collected 243 records.",
-        "cowrie.session.file_download: Collected 111 records.",
-        "cowrie.session.closed: Collected 7973 records.",
-        "cowrie.login.success: Collected 3531 records.",
-        "cowrie.login.failed: Collected 2316 records.",
-        "cowrie.log.closed: Collected 1839 records.",
-        "cowrie.direct-tcpip.request: Collected 4388 records."
-
-    ]
-     return render(request, 'home.html', {'marquee_left': marquee_left, 'marquee_right': marquee_right})
+    type_data = ClassificationHistory.objects.values('attack_type') \
+    .annotate(count=Count('attack_type'))
+    type_counts = pd.Series({item['attack_type']: item['count'] for item in type_data}).sort_values(ascending=False)
+    
+    marquee_left = []
+    for att_type, att_count in type_counts.items():
+        marquee_left.append(f'{att_type}: Collected {att_count} records.')
+        
+    marquee_left.append("Collected 643 records in the past 7 days.")
+    marquee_left.append("cowrie.session.connect: Collected 8419 records.")
+    marquee_right = marquee_left[::-1]
+    
+    return render(request, 'home.html', {'marquee_left': marquee_left, 'marquee_right': marquee_right})
 
 
 def about_view(request):
